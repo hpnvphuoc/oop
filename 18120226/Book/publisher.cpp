9e31e88cdd42100016b8718edcb908062e167a1b
Nguyen Van Phuoc <hpnvphuoc@gmail.com>
2019-11-13 15:49:13 +0700

    find cutstring

--- 18120226/Book/publisher.cpp
+++ 18120226/Book/publisher.cpp
@@ -6,7 +6,34 @@ publisher::publisher()
 {
 }
 
-
 publisher::~publisher()
 {
+}
+
+void listBook::readListBookFromFile()
+{
+	string tempNumkind;
+	string tempBook;
+	string tempIBSN;
+	string tempName;
+	int tempPrice;
+	string tempPublisher;
+	int tempNumber;
+	string tempAuthor;
+	fstream f;
+	f.open("listBook.txt", ios::in);
+	getline(f, tempNumkind);
+	numKind = stoi(tempNumkind);
+	this->list = new book[numKind];
+	for(int i=0;i<numKind;i++){
+		getline(f,tempBook);
+		tempIBSN = cutstring(tempBook);
+		tempName = cutstring(tempBook);
+		tempPrice = stoi(cutstring(tempBook));
+		tempPublisher = cutstring(tempBook);
+		tempNumber = stoi(cutstring(tempBook));
+		tempAuthor = cutstring(tempBook);
+		list[i].set(tempIBSN, tempName, tempPrice, tempPublisher, tempNumber, tempAuthor);
+	}
+	f.close();
 }
